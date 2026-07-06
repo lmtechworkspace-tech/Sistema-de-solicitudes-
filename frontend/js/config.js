4710d065a20bcd332a0d5e698f9f152a9deee8c2
Luis Cesar <luis11cesar11lm@gmail.com>
2026-07-06 10:55:25 -0400

    config: URLs reales de Intake/Backoffice desplegados

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -6,7 +6,7 @@
  * reemplaza por el ID real al desplegar cada Web App (checklist §17.2).
  */
 window.SIGSO_CONFIG = Object.freeze({
-  INTAKE_URL: 'https://script.google.com/macros/s/REEMPLAZAR_ID_PUB/exec',
-  BACKOFFICE_URL: 'https://script.google.com/macros/s/REEMPLAZAR_ID_MGMT/exec',
+  INTAKE_URL: 'https://script.google.com/macros/s/AKfycbypI38IfuisU2DFMnvM9_knsbqgm8T-9rnkUUr5MbLlc5_J7BZuXhy8mZC-GtVHzEV9aA/exec',
+  BACKOFFICE_URL: 'https://script.google.com/macros/s/AKfycbzoC2IsvrwPIElUeTgIxmNxLcNsEH3SXU8TrKLM-sFntZjd8ratSv8w_1-zGo1MmdCcFg/exec',
   TIMEZONE: 'America/Santiago'
 });
