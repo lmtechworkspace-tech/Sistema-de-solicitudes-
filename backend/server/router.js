8500f02696b8d0e6710e18e2ef55b8ad9fcf51b8
Luis Cesar <luis11cesar11lm@gmail.com>
2026-09-16 22:59:58 -0300

    Revert "feat(ecosistema): re-agregar bootstrapAdmin temporalmente (resetear clave real)"
    This reverts commit 42aeec915407b7cb0f26e84c16ab1e6ea8ebaf2d.

--- backend/server/router.js
+++ backend/server/router.js
@@ -35,7 +35,6 @@ const Jefatura = require('../logica/jefatura');
 const Dashboard = require('../logica/dashboard');
 const Gerencia = require('../logica/gerencia');
 const Notificaciones = require('../logica/notificaciones');
-const Bootstrap = require('../logica/bootstrap');
 
 // Acciones que NO requieren una sesion ya resuelta: o bien la crean
 // (portalLogin), o bien resuelven su propio token internamente y devuelven
@@ -49,10 +48,7 @@ const ACCIONES_PUBLICAS = new Set([
   'portalLogin', 'portalLogout', 'portalSesion', 'portalCambiarPassword', 'crearSolicitud',
   'consultarEstado', 'solicitarCodigoAcceso', 'misSolicitudes',
   'editarSubsolicitud', 'eliminarArchivo', 'responderConsulta', 'validarCierre',
-  'getCatalogos',
-  // TEMPORAL (bootstrap.js): sin sesion ADM que la proteja a proposito --
-  // la protege el secreto de servidor (MIGRACION_BOOTSTRAP_SECRET).
-  'bootstrapAdmin'
+  'getCatalogos'
 ]);
 
 const ACCIONES = {
@@ -94,10 +90,7 @@ const ACCIONES = {
   getPanelGerencia: (db, data, contexto) => Gerencia.getPanel(db, data, contexto),
   getPanelJefatura: (db, data, contexto) => Jefatura.getPanel(db, data, contexto),
 
-  listarLogsNotificaciones: (db, data, contexto) => Notificaciones.listarLogs(db, data, contexto),
-
-  // TEMPORAL (bootstrap.js): crear/resetear la cuenta ADM para poder arrancar la migracion.
-  bootstrapAdmin: (db, data) => Bootstrap.bootstrapAdmin(db, data)
+  listarLogsNotificaciones: (db, data, contexto) => Notificaciones.listarLogs(db, data, contexto)
 };
 
 function responderResultado_(resultado) {
